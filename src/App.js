a27c788f91286d1c0a32950ffeb0c2d81099e0bc
Michael Baez <mrbaez427@gmail.com>
2023-03-02 22:48:49 -0500

    Social footer complete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Navbar";
 import About from "./About";
-import Footer from "./Footer";
+import FooterSocials from "./FooterSocials";
+import FooterEmail from "./FooterEmail";
 
 function App() {
   return (
     <div className="text-text bg-main w-full h-full flex flex-col min-h-screen">
       <Navbar />
-      <div className="ml-28 mt-44 h-screen flex-grow">
-        <h2 className="text-xl text-pink-500 font-mono my-4">Hi, my name is</h2>
-        <h1 className="text-6xl font-sans font-bold">Michael Baez.</h1>
-        <h1 className="text-6xl text-darktext font-sans font-bold">
-          And I am a full-stack web developer.
-        </h1>
-        <p className="text-darktext text-xl mt-6">
-          I'm a software engineer that specializes in building web applications.
-        </p>
-        <a href="PLACEHOLDER">
-          <h2 className="text-xl text-pink-500 font-mono my-12 w-max p-4 border  rounded border-pink-500 hover:bg-pink-500 hover:bg-opacity-20">
-            Check out my work!
+      <div className="flex flex-row h-screen">
+        <FooterSocials />
+        <div className="ml-28 mt-44 flex-row">
+          <h2 className="text-xl text-pink-500 font-mono my-4">
+            Hi, my name is
           </h2>
-        </a>
+          <h1 className="text-6xl font-sans font-bold">Michael Baez.</h1>
+          <h1 className="text-6xl text-darktext font-sans font-bold">
+            And I am a full-stack web developer.
+          </h1>
+          <p className="text-darktext text-xl mt-6">
+            I'm a software engineer that specializes in building web
+            applications.
+          </p>
+          <a href="PLACEHOLDER">
+            <h2 className="text-xl text-pink-500 font-mono my-12 w-max p-4 border  rounded border-pink-500 hover:bg-pink-500 hover:bg-opacity-20">
+              Check out my work!
+            </h2>
+          </a>
+        </div>
+        <FooterEmail />
       </div>
 
       <About />
-      <Footer />
     </div>
   );
 }
---
+++ src/FooterEmail.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class FooterEmail extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="text-text fixed bottom-0 ">
+        <a href="PLACEHOLDER">mrbaez427@gmail.com</a>
+        <hr className="w-px h-32 mx-auto bg-darktext"></hr>
+      </div>
+    );
+  }
+}
+
+export default FooterEmail;
